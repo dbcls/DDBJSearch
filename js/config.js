7fa305fb61755d369b8f8e79b2dd9116d1b14e1c
Tazro Inutano Ohta <inutano@gmail.com>
2018-08-22 18:10:47 +0900

    default values

--- js/config.js
+++ js/config.js
@@ -5,4 +5,8 @@ var conf = {
     api_details_base_url : "http://52.193.211.138/api/details?db=",
     api_update_url: "http://52.193.211.138/api",
     api_chart_url: "http://52.193.211.138/api"
+    api_search_base_url: "http://54.249.69.50/api/search?",
+    api_details_base_url : "http://54.249.69.50/api/details?db=",
+    api_update_url: "http://54.249.69.50/api",
+    api_chart_url: "http://54.249.69.50/api"
 }
